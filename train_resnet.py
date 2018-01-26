bddf4d12c0a3331e61950b3d6e08a61c34288ae8
prlz77 <pau.rodri1@gmail.com>
2018-01-26 12:53:21 +0100

    fix

--- train_resnet.py
+++ train_resnet.py
@@ -88,8 +88,7 @@ def main(args):
             else:
                 loss = 0
                 output = model(data)
-            loss = criterion(output, label)
-            print(loss)
+            loss += criterion(output, label).mean()
             loss.backward()
             optimizer.step()
             train_loss_meter.update(loss.data[0], data.size(0))
@@ -107,7 +106,7 @@ def main(args):
                 output, loss = model(data)
             else:
                 output = model(data)
-            loss = criterion(output, label)
+            loss = criterion(output, label).mean()
             val_loss_meter.update(loss.data[0], data.size(0))
             preds = output.max(1)[1]
             val_accuracy_meter.update((preds == label).float().sum().data[0], data.size(0))
