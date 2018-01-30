96a586a2a896ccab2e5a5e565fef57c5647b0164
prlz77 <pau.rodri1@gmail.com>
2018-01-30 12:29:35 +0100

    remove conv1x1

--- modules/attention.py
+++ modules/attention.py
@@ -147,7 +147,7 @@ class AttentionModule(torch.nn.Module):
         super(AttentionModule, self).__init__()
         # self.conv_reduce = torch.nn.Conv2d(in_ch, in_ch // 2, kernel_size=1, stride=1, padding=0, bias=False)
         # self.conv_bn = torch.nn.BatchNorm2d(in_ch // 2)
-        # self.in_ch = in_ch // 2
+        self.in_ch = in_ch #// 2
         self.nlabels = nlabels
         self.nheads = nheads
         self.has_gates = has_gates
