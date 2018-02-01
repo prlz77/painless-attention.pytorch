19e78ecc7c69a69e18c345cefc1f02f37b38c0f3
prlz77 <pau.rodri1@gmail.com>
2018-02-01 19:39:51 +0100

    simplification

--- modules/attention.py
+++ modules/attention.py
@@ -50,7 +50,6 @@ class AttentionHead(torch.nn.Module):
         self.nheads = nheads
         self.conv = torch.nn.Conv2d(in_ch, nheads, kernel_size=3, padding=1, bias=False)
         torch.nn.init.kaiming_normal(self.conv.weight.data)
-        self.bn = torch.nn.BatchNorm2d(in_ch)
         self.register_buffer("diag",
                              torch.from_numpy(
                                  1 - np.eye(self.nheads, self.nheads).reshape(1, self.nheads, self.nheads)).float())
@@ -77,9 +76,36 @@ class AttentionHead(torch.nn.Module):
         """
         b, c, h, w = x.size()
         self.att_mask = F.softmax(self.conv(x).view(b, self.nheads, w * h), 2).view(b, self.nheads, h, w)
-        xatt_mask = x.view(b, 1, c, h, w) * self.att_mask.view(b, self.nheads, 1, h, w)
-        return self.bn(xatt_mask.view(b * self.nheads, c, h * w))
+        return self.att_mask
 
+class OutHead(torch.nn.Module):
+    """ Attention Heads
+
+    Attentds a given feature map. Provides inter-mask regularization.
+    """
+
+    def __init__(self, in_ch, out_ch):
+        """ Constructor
+
+        Args:
+            in_ch: input feature map channels
+            nheads: number of attention masks
+        """
+        super(OutHead, self).__init__()
+        self.conv = torch.nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1, bias=False)
+        torch.nn.init.kaiming_normal(self.conv.weight.data)
+        self.bn = torch.nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        """ Pytorch Forward
+
+        Args:
+            x: input feature map
+
+        Returns: the multiple attended feature maps
+
+        """
+        return self.bn(self.conv(x))
 
 class AttentionModule(torch.nn.Module):
     """ Attention Module
@@ -104,8 +130,9 @@ class AttentionModule(torch.nn.Module):
         self.nlabels = nlabels
         self.nheads = nheads
         self.reg_w = reg_w
-        self.att_head = torch.nn.Conv2d(in_ch, nheads, kernel_size=3, stride=1, padding=1, bias=False)
-        self.out_head = torch.nn.Conv2d(in_ch, nlabels, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.att_head = AttentionHead(in_ch, nheads)
+        self.out_head = OutHead(in_ch, nlabels*nheads)
 
 
     def reg_loss(self):
@@ -125,8 +152,9 @@ class AttentionModule(torch.nn.Module):
         Returns: tuple with predictions and gates. Gets are set to None if disabled.
 
         """
-        att_mask = F.softmax(self.att_head(x).view(x.size(0), self.nheads, 1, x.size(2) * x.size(3)), 3)
-        output = self.out_head(x)
+        b, c, h, w = x.size()
+        att_mask = self.att_head(x)
+        output = self.out_head(x.view(b, 1, c, h, w)) * att_mask.view(b, self.nheads, 1, h, w)
         output = (output.view(x.size(0), 1, x.size(1), x.size(2)*x.size(3)) * att_mask).sum(3)
         return output
 
